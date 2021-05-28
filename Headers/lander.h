8ae278f0b790b7041bb73e6a0f3abfa57f7c73dd
DevPrestigious <66549514+DevPrestigious@users.noreply.github.com>
2021-05-27 19:55:12 -0600

    Created lander.h
    Lander.h needs to be "Classed"

---
+++ Headers/lander.h
@@ -0,0 +1,18 @@
+//
+//  lander.h
+//  lunarLander
+//
+//  Created by Alexander Dohms on 5/27/21.
+//
+
+#ifndef lander_h
+#define lander_h
+
+class lander {
+private:
+   
+public:
+   <#member functions#>
+};
+
+#endif /* lander_h */
